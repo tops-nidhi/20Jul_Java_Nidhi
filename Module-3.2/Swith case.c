0916036c8a088575e64eb4ad9df64d61463892fa
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2022-08-29 18:47:43 +0530

    add

--- Module-3.2/Swith case.c
+++ Module-3.2/Swith case.c
@@ -2,7 +2,7 @@
 void main()
 {
     int no1,no2;
-    int ch;
+    int ch,ch1;
     printf("Enter 2 no:");
     scanf("%d%d",&no1,&no2);
     printf("\n1.add\n2.mul\n3.sub\n");
@@ -11,7 +11,18 @@ void main()
     switch (ch)
     {
         case 1:
-            printf("\nAdd is:%d",no1+no2); 
+        printf("\nEnter your choice:");
+        scanf("%d",&ch1);
+            switch(ch1)
+            {
+                case 1:
+                    printf("\nThis is Nested Switch case 1.");
+                    break;
+                case 2:
+                    printf("\nThis is Nested switch case 2.");
+                    break;
+            }
+             printf("\nAdd is:%d",no1+no2); 
             break;
         case 2:
             printf("\nMul is:%d",no1*no2);
