8ae94ee694e86526a45008a2d9f9688b232ff86d
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2022-09-01 16:09:59 +0530

    add

--- Module-3.2/For_Loop.c
+++ Module-3.2/For_Loop.c
@@ -4,6 +4,12 @@ void main()
     int i;
     for(i = 10; i > 0; i--)
     {
+        if(i == 5)
+        {
+            goto khyati;
+        }
         printf("\n%d",i);
     }
+    khyati:
+        printf("\nThis is goto statement.........");
 }
